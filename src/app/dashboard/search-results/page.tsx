bce114441d70ad4ee7607ffb00a5ec75d226f6ab
ngavilanes <nicholas.gavilanes@gmail.com>
2025-05-06 00:01:40 -0400

    fixed useParams hook

--- src/app/dashboard/search-results/page.tsx
+++ src/app/dashboard/search-results/page.tsx
@@ -1,15 +1,11 @@
 'use client';
 import SearchContainer from '@/components/search-results/search-container';
-import { useSearchParams } from 'next/navigation';
 import { Suspense } from 'react';
 
 export default function SearchResultsPage() {
-  const searchParams = useSearchParams();
-  const searchQuery = searchParams.get('search') || '';
-
   return (
     <Suspense>
-      <SearchContainer roleProp={searchQuery}></SearchContainer>
+      <SearchContainer />
     </Suspense>
   );
 }
--- src/components/search-results/search-container.tsx
+++ src/components/search-results/search-container.tsx
@@ -5,18 +5,19 @@ import { Input } from '../ui/input';
 import { LocateFixedIcon, Search } from 'lucide-react';
 import JobFilters from './job-filters';
 import { Button } from '../ui/button';
+import { useSearchParams } from 'next/navigation';
 
-interface ContainerProps {
-  roleProp?: string;
-}
-const SearchContainer = ({ roleProp }: ContainerProps) => {
-  const [searchRole, setSearchRole] = useState<string>(roleProp || '');
+
+const SearchContainer = () => {
+   const searchParams = useSearchParams();
+    const searchQuery = searchParams.get('search') || '';
+  const [searchRole, setSearchRole] = useState<string>(searchQuery || '');
   const [searchLocation, setSearchLocation] = useState<string>('');
 
   return (
     <div>
       <div className="flex items-center justify-center gap-x-2 relative">
-        <Search className="absolute left-148 text-muted-foreground" />
+        <Search className="absolute left-150 text-muted-foreground" />
         <Input
           type="text"
           placeholder="Job Role..."
@@ -24,7 +25,7 @@ const SearchContainer = ({ roleProp }: ContainerProps) => {
           onChange={(e) => setSearchRole(e.target.value)}
           className="w-1/8 pl-10 pr-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
-        <LocateFixedIcon className="absolute left-202 text-muted-foreground" />
+        <LocateFixedIcon className="absolute left-205 text-muted-foreground" />
         <Input
           type="text"
           placeholder="City, state, or zip code..."
