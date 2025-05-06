d4a6ad2e100f1c77851b5bcbd291f32b7bfe33ae
ngavilanes <nicholas.gavilanes@gmail.com>
2025-05-05 23:55:44 -0400

    fixed useParams hook

--- src/app/dashboard/search-results/page.tsx
+++ src/app/dashboard/search-results/page.tsx
@@ -1,10 +1,15 @@
 'use client';
 import SearchContainer from '@/components/search-results/search-container';
 import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 
 export default function SearchResultsPage() {
   const searchParams = useSearchParams();
   const searchQuery = searchParams.get('search') || '';
-  
-  return <SearchContainer roleProp={searchQuery}></SearchContainer>;
+
+  return (
+    <Suspense>
+      <SearchContainer roleProp={searchQuery}></SearchContainer>
+    </Suspense>
+  );
 }
